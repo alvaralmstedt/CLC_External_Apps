38db23f2bbb986a0088456ad4c85787bb71a3300
alvaralmstedt <alvar.almstedt@gmail.com>
2017-05-09 11:42:02 +0200

    Addmin bam_fixer_2.py in place of bam_fixer. First commit on that.

---
+++ bam_fixer_2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from sys import argv
+import subprocess
+from os import path
+
+"""
+This script will take bam or sam files from CLC and try to convert them to a format that is more compatible with
+third part software that wants more bwa-like bam/sam files (such as manta for example). Work In Progress.
+"""
+
+def sam_split(samfile_in, out_perfect, out_secondary):
+    with open(samfile_in) as sam_in:
+        with open(out_perfect, "w+") as perfect:
+            with open(out_secondary, "w+") as secondary:
+                for line in sam:
+                    old_line = line.split("\t")
+                    #new_line = old_line
+                    NH_field = old_line[12].split(":")[-1].rstrip()
+                    if int(old_line[4]) <= 3 and int(NH_field) > 1:
+                        secondary.write(line)
+                        #new_line[1] = str(int(old_line[4]) + 256)
+                    elif not line.startswith("@"):
+                        perfect.write(line)
+                    else:
+                        print("This line will not be used:")
+                        print(line)
+                    #new_line = "\t".join(new_line)
+                    #print new_line
+                    #yield str(new_line)
+
+if __name__ == "__main__":
+    infile = argv[1]
+    outfile_perfect = argv[2]
+    outfile_secondary = argv[3]
+    fasta_index = "/medstore/External_References/hg19/Homo_sapiens_sequence_hg19.fasta.fai"
+    bwa_index = fasta_index.rsplit('.', 1)[0]
+    directory = path.dirname(outfile_secondary)
+
+    call("module load samtools/1.3.1", shell=True)
+    call("module load bwa/0.7.5a", shell=True)
+    if ".bam" in infile:
+        intermediary = infile.replace(".bam", ".sam")
+        subprocess.call("samtools view %s -o %s -@ 8" % (infile, intermediary))
+        sam_split(intermediary, outfile_perfect, outfile_secondary)
+    elif ".sam" in infile:
+        sam_split(infile, outfile_perfect, outfile_secondary)
+
+    #headers = str(subprocess.call("tr '\t' '\n' < %s | grep RG: | sort | uniq" % outfile_secondary, shell=True, stdout=subprocess.PIPE))
+    #with open(outfile_secondary + "tmp", "w+") as sec_tmp:
+    #    sec_tmp.write(headers)
+    #    sec_tmp.write(outfile_secondary)
+    #    subprocess.call("mv %s %s" % (sec_tmp, outfile_secondary))
+    secondary_tmp = outfile_secondary + "_temp"
+    subprocess.call("samtools view -ht %s %s > %s" % (fasta_index, outfile_secondary, secondary_tmp), shell=True)
+    subprocess.call(["mv", secondary_tmp, outfile_secondary])
+    subprocess.call("samtools fastq %s > %s/reads_interleaved.fastq" % (outfile_secondary, directory), shell=True)
+    subprocess.call(["bwa", "mem", bwa_index, "-p", directory + "/reads_interleaved.fastq", "-t", "40", ">",
+                     directory + "/bwa_out.sam"])
+    bwa_out = directory + "/bwa_out.sam"
+    merged_bam = directory + "/merged.bam"
+    sorted_bam = directory + "/merged_sorted.bam"
+    subprocess.call(["cat", outfile_perfect, ">>", bwa_out])
+    subprocess.call(["samtools", "view", "-@", "40", bwa_out, "-o", merged_bam])
+    subprocess.call(["samtools", "sort", "-@", "-20", "-m", "2G", merged_bam, ">", sorted_bam])
+    subprocees.call(["samtools", "index", sorted_bam])
